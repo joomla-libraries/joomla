1535def7b1212972d397b9ea511a62f68e47cc1e
Louis Landry <louis.landry@joomla.org>
2006-03-24 09:15:44 +0000

    What is this? MVC in com_content? wow!!! [WIP]
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@2889 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- application/view.php
+++ application/view.php
@@ -27,7 +27,7 @@ class JView extends JObject {
 	var $_viewName = null;
 
 	/** @var array Internal data array */
-	var $_viewData = null;
+	var $_models = null;
 
 	var $_controller = null;
 
@@ -38,55 +38,116 @@ class JView extends JObject {
 		$this->_viewData = array();
 	}
 
-	function &setController( &$controller ) {
-		$this->_controller = &$controller;
-		return $controller;
-	}
-
-	function &getController() {
-		return $this->_controller;
-	}
-
-	function setViewName( $value ) {
-		// clean name
-		$value = preg_replace( '#\W#', '', $value );
-		$this->_viewName = $value;
+	/**
+	 * Method to set the name of the view.  In most cases this will only be used
+	 * by the constructor, but is provided as a public method for flexibility.
+	 * 
+	 * @access	public
+	 * @param	string	$name	New view name
+	 * @return	string	New view name
+	 * @since	1.5
+	 */
+	function setViewName( $name ) {
+		// Clean and set the view name
+		$this->_viewName = preg_replace( '#\W#', '', $name );
 		return $this->_viewName;
 	}
 
+	/**
+	 * Method to get the name of the view.
+	 * 
+	 * @access	public
+	 * @return	string	The name of the view
+	 * @since	1.5
+	 */
 	function getViewName() {
 		return $this->_viewName;
 	}
 
 	/**
-	 * Data setter
-	 * @param string The name of the data variable
-	 * @param mixed The value of the data variable
+	 * Method to set the controller object for the view.  In most cases this
+	 * will only be used by the constructor, but is provided as a public method
+	 * for flexibility.
+	 * 
+	 * @access	public
+	 * @param	object	$controller	The view's controller
+	 * @return	object	The controller
+	 * @since	1.5
 	 */
-	function &setVar( $name, &$value ) {
-		$this->_viewData[$name] = &$value;
-		return $value;
+	function &setController( &$controller ) {
+		$this->_controller = &$controller;
+		return $controller;
 	}
 
 	/**
-	 * Data getter
-	 * @param string The name of the data variable
-	 * @return mixed The value of the data variable
+	 * Method to get the view's controller object.
+	 * 
+	 * @access	public
+	 * @return	object	The controller
+	 * @since	1.5
 	 */
-	function &getVar( $name ) {
-		if (isset( $this->_viewData[$name] )) {
-			return $this->_viewData[$name];
-		} else {
-			$null = null;
-			return $null;
+	function &getController() {
+		return $this->_controller;
+	}
+
+	/**
+	 * Method to add a model to the view.  We support a multiple model single
+	 * view system by which models are referenced by classname.  A caveat to the
+	 * classname referencing is that any classname prepended by JModel will be
+	 * referenced by the name without JModel, eg. JModelCategory is just
+	 * Category.
+	 * 
+	 * @access	public
+	 * @param	object		$model		The model to add to the view.
+	 * @param	boolean	$default	Is this the default model?
+	 * @return	object		The added model
+	 * @since	1.5
+	 */
+	function &setModel( &$model, $default = false ) {
+		$name = strtolower(get_class($model));
+		$name = substr($name, strpos($name, 'jmodel') ? strpos($name, 'jmodel')+6 : 0);
+		$this->_models[$name] = &$model;
+		if ($default) {
+			$this->_defaultModel = $name;
 		}
+		return $model;
 	}
 
 	/**
-	 * @abstract
+	 * Method to get data from a registered model
+	 * 
+	 * @access	public
+	 * @param	string	The name of the method to call on the model
+	 * @param	string	The name of the model to reference [optional]
+	 * @return mixed	The return value of the method
+	 * @since	1.5
 	 */
-	function getData() {
-		return array();
+	function &get( $method, $model = null ) {
+
+		// If $model is null we use the default model
+		if (is_null($model)) {
+			$model = $this->_defaultModel;
+		} 
+
+		// First check to make sure the model requested exists
+		if (isset( $this->_model[$model] )) {
+			// Model exists, lets build the method name
+			$method = 'get'.ucfirst($method);
+
+			// Does the method exist?
+			if (method_exists($this->_model[$model], $method)) {
+				// The method exists, lets call it and return what we get
+				return $this->_model[$model]->$method();
+			} else {
+				// Method wasn't found... throw a warning and return false
+				JError::raiseWarning( 0, 'Unknown Method', "$model::$method() was not found");
+				return false;
+			}
+		} else {
+			// Model wasn't found, return throw a warning and return false
+			JError::raiseWarning( 0, 'Unknown Model', "$model model was not found");
+			return false;
+		}
 	}
 
 	/**
@@ -95,6 +156,5 @@ class JView extends JObject {
 	function display() {
 		JError::raiseNotice( 0, 'Display method not set in this class' ); 
 	}
-
 }
 ?>
